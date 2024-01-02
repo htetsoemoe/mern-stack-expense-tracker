c9cfc26d829bd896602b9849b58bec9ec48105b7
htetsoemoe <soemoehtet.mdy@gmail.com>
2024-01-02 16:48:15 +0630

    created mongodb cluster and connected to cluster

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const PORT = process.env.PORT || 3500
+
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
