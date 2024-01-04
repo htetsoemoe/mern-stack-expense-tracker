84fb54364efd320d32e73a92304a225ed33eddaf
htetsoemoe <soemoehtet.mdy@gmail.com>
2024-01-04 16:30:38 +0630

    basic router level middleware

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,19 @@ const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
 const mongoose = require('mongoose')
 const connectDB = require('./config/dbConfig')
+const { readdirSync } = require('fs')
 require('dotenv').config()
 
 const PORT = process.env.PORT || 3500
 
 connectDB()
 
+// middlewares
 app.use(express.json())
 app.use(cors(corsOptions))
+
+// router-level middleware
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
 mongoose.connection.once('open', () => {
     console.log("Connected to MongoDB")
