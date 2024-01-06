cc29b8f33b7329bf80aa403ac20826324355bf02
htetsoemoe <soemoehtet.mdy@gmail.com>
2024-01-06 11:22:36 +0630

    layouts for app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import bg from './img/bg.png'
+import styled from 'styled-components'
+import { MainLayout } from './styles/Layouts'
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <AppStyled bg={bg}>
+      <MainLayout>
+        <h1>Hello World</h1>
+      </MainLayout>
+    </AppStyled>
   )
 }
+
+// Created 'Styled-Component'
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: url(${props => props.bg});
+  position: relative;
+`
 
 export default App
