262e7020b86ed1f51609f4a6cf344a66e0c930b9
htetsoemoe <soemoehtet.mdy@gmail.com>
2024-01-13 12:09:18 +0630

    get all income with axios on income component

---
+++ frontend/src/context/globalContext.jsx
@@ -0,0 +1,54 @@
+import React, { useContext, useState } from "react";
+import axios from 'axios'
+
+const BASE_URL = "http://localhost:3500/api/v1/"
+
+const GlobalContext = React.createContext()
+
+export const GlobalProvider = ({ children }) => {
+
+    const [incomes, setIncomes] = useState([])
+    const [expenses, setExpenses] = useState([])
+    const [error, setError] = useState(null)
+
+    const addIncome = async (income) => {
+        const response = await axios.post(`${BASE_URL}add-income`, income)
+            .catch((err) => {
+                setError(err.response.data.message)
+            })
+    }
+
+    const getIncomes = async () => {
+        const response = await axios.get(`${BASE_URL}all-incomes`)
+        setIncomes(response.data)
+        console.log(incomes)
+    }
+
+    const deleteIncome = async (id) => {
+        const response = await axios.delete(`${BASE_URL}delete-income/${id}`)
+    }
+
+    const totalIncome = () => {
+        let totalIncome = 0;
+        incomes.forEach((income) => {
+            totalIncome = totalIncome + income.amount
+        })
+        return totalIncome
+    }
+
+    return (
+        <GlobalContext.Provider value={{
+            addIncome,
+            getIncomes,
+            incomes,
+            totalIncome,
+            deleteIncome,
+        }}>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
+
+export const useGlobalContext = () => {
+    return useContext(GlobalContext)
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { GlobalStyle } from './styles/GlobalStyle.js'
 import { BrowserRouter } from 'react-router-dom'
+import { GlobalProvider } from './context/globalContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <GlobalStyle />
-      <App />
+      <GlobalProvider>
+        <App />
+      </GlobalProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
